e8cffe3802c83469291f3c0869cd26c2e3a5cade
nchirkov <chirkov.nikita@gmail.com>
2016-10-09 21:44:45 +0300

    Add service for getting data

--- MailBox/app.js
+++ MailBox/app.js
@@ -1,4 +1,10 @@
   angular.module('mailBoxApp', [])
+      .service('MailService', function($http) {
+          this.get = () => {
+              return $http.get('http://test-api.javascript.ru/v1/nchirkov/letters')
+                  .then(response => response.data)
+          }
+      })
       .component('mailitem', {
           bindings: {
               item: '<'
@@ -6,26 +12,8 @@
           templateUrl: 'mailItem.tpl.html'
       })
       .component('maildata', {
-          controller: function() {
-              this.items = [{
-                  "id": 1,
-                  "from": "inbox@mail.ru",
-                  "subject": "Регистрация на событие",
-                  "date": "2015-04-23T18:25:43.511Z",
-                  "content": "Вы только что зарегистрировались на событие. Подтверждаем, что Ваша регистрация прошла."
-              }, {
-                  "id": 2,
-                  "from": "welcome@gmail.com",
-                  "subject": "Новое спецпредложение",
-                  "date": "2016-04-23T17:22:43.511Z",
-                  "content": "Покупайте билеты на любые фильмы через приложение."
-              }, {
-                  "id": 3,
-                  "from": "list@facebook.com",
-                  "subject": "Сегодня празднует день рождения Иван",
-                  "date": "2016-10-01T18:25:43.511Z",
-                  "content": "Иван отмечает свой день рождения 1 октября. Поздравьте!"
-              }];
+          controller: function(MailService) {
+              MailService.get().then(items => this.items = items);
           },
           templateUrl: 'maildata.tpl.html'
       });
