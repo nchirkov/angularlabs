60505d94a0fb2e59c3d4cf517a42e2e28f0f3f8d
nchirkov <chirkov.nikita@gmail.com>
2016-10-10 20:47:06 +0300

    Add dropdowns for currency

--- CurrencyConverter/app.js
+++ CurrencyConverter/app.js
@@ -1,13 +1,31 @@
 angular.module('currencyConverterApp', [])
     .service('ConverterService', function() {
-        this.convert = (from) => {
-            return from * 63
+        let rates = {
+            "USD": {
+                "RUB": 63,
+                "USD": 1,
+                "EUR": 1 / 1.11977
+            },
+            "RUB": {
+                "RUB": 1,
+                "USD": 1 / 63,
+                "EUR": 1 / 70
+            },
+            "EUR": {
+                "RUB": 70,
+                "USD": 1.11977,
+                "EUR": 1
+            }
+        };
+
+        this.convert = (fromAmount, fromCurrency, toCurrency) => {
+            return fromAmount * rates[fromCurrency][toCurrency];
         }
     })
     .component('converter', {
         controller: function(ConverterService) {
             this.convert = () => {
-                this.toAmount = ConverterService.convert(this.fromAmount);
+                this.toAmount = ConverterService.convert(this.fromAmount, this.fromCurrency, this.toCurrency);
             };
         },
         templateUrl: 'converter.tpl.html'
