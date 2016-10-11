a27de9184ffea54c1a62d37248f16aec37fb6a7a
nchirkov <chirkov.nikita@gmail.com>
2016-10-12 00:41:33 +0300

    Use service for getting data

--- UserCards/app.js
+++ UserCards/app.js
@@ -1,4 +1,10 @@
 angular.module('cardApp', [])
+    .service('UserService', function($http) {
+        this.get = () => {
+            return $http.get('https://test-api.javascript.ru/v1/nchirkov/users')
+                .then(response => response.data)
+        }
+    })
     .component('usercard', {
         bindings: {
             user: '<'
@@ -6,32 +12,8 @@ angular.module('cardApp', [])
         templateUrl: 'usercard.tpl.html'
     })
     .component('userdata', {
-        controller: function() {
-            this.users = [{
-                "id": 1,
-                "fullName": "Иванов Иван",
-                "avatarUrl": "https://randomuser.me/api/portraits/thumb/men/57.jpg",
-                "birthdate": "1976-10-10",
-                "gender": "мужской",
-                "address": "ул. Звенигороская, 47б",
-                "email": "ivanov@mail.ru"
-            }, {
-                "id": 2,
-                "fullName": "Петров Петр",
-                "avatarUrl": "https://randomuser.me/api/portraits/thumb/men/7.jpg",
-                "birthdate": "1957-01-14",
-                "gender": "мужской",
-                "address": "ул.Пушкиская, 13",
-                "email": "ivanov@mail.ru"
-            }, {
-                "id": 3,
-                "fullName": "Натальина Наталья",
-                "avatarUrl": "https://randomuser.me/api/portraits/thumb/women/7.jpg",
-                "birthdate": "1990-07-03",
-                "gender": "женский",
-                "address": "ул. Лермонтова, 59",
-                "email": "ivanov@mail.ru"
-            }]
+        controller: function(UserService) {
+            UserService.get().then(users => this.users = users);
         },
         templateUrl: 'userdata.tpl.html'
     });
